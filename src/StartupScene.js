a1cd80bd2c8300e9bcd1b5b8541232360eefcfc6
newagebegins <newagebegins@gmail.com>
2012-05-31 18:53:53 +1100

    add title and controls help

--- src/StartupScene.js
+++ src/StartupScene.js
@@ -14,5 +14,19 @@ StartupScene.prototype.tick = function () {
 };
 
 StartupScene.prototype.draw = function (ctx) {
+  this._drawTitle(ctx);
+  this._drawControlsHelp(ctx);
   this._pressEnterText.draw(ctx);
+};
+
+StartupScene.prototype._drawTitle = function (ctx) {
+  ctx.fillStyle = "#ffff00";
+  ctx.font = "bold 90px 'Lucida Console', Monaco, monospace"
+  ctx.fillText("PAC-MAN", 76, 150);
+};
+
+StartupScene.prototype._drawControlsHelp = function (ctx) {
+  ctx.fillStyle = "#dedede";
+  ctx.font = "bold 14px 'Lucida Console', Monaco, monospace"
+  ctx.fillText("CONTROL: ARROW KEYS", 187, 300);
 };
